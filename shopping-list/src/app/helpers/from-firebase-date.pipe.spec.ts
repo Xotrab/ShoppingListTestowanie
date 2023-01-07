fe3aaba2112bad77a4e1b28b7f00fd9a86e70f3f
Xotrab <mokrylisc@gmail.com>
2023-01-07 14:08:14 +0100

    Added the unit tests for the from firebase date pipe

--- shopping-list/src/app/helpers/from-firebase-date.pipe.spec.ts
+++ shopping-list/src/app/helpers/from-firebase-date.pipe.spec.ts
@@ -1,8 +1,20 @@
+import { Timestamp } from '@angular/fire/firestore';
 import { FromFirebaseDatePipe } from './from-firebase-date.pipe';
 
-describe('FromFirebaseDatePipe', () => {
+fdescribe('FromFirebaseDatePipe', () => {
+  const pipe = new FromFirebaseDatePipe();
+
   it('create an instance', () => {
-    const pipe = new FromFirebaseDatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms firebase timestamp to miliseconds', () => {
+    const input = new Timestamp(1000,0);
+    expect(pipe.transform(input)).toBe(1000000);
+  });
+
+  it('returns the input unchanged if it is not a firebase timestamp', () => {
+    const input = new Date();
+    expect(pipe.transform(input)).toBe(input);
   });
 });
